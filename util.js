fe6d6252c4b12d95bd202d3d0b181d766e0026de
Jay Adams <46606151+jwadams26@users.noreply.github.com>
2019-12-07 13:57:04 -0500

    moved working project to index

--- util.js
+++ util.js
@@ -2,11 +2,11 @@ function loadObj( path, name) {
     var obj;
     var mtlLoader = new THREE.MTLLoader();
     mtlLoader.setPath( path );
-    mtlLoader.load( name+".mtl", function( mat ) {
-        mat.preload();
+    mtlLoader.load( name+".mtl", function( material ) {
+        material.preload();
 
         var objLoader = new THREE.OBJLoader();
-        objLoader.setMaterials( mat );
+        objLoader.setMaterials( material );
         objLoader.setPath( path );
         objLoader.load( name + ".obj", function( object ) {
             obj = object;
